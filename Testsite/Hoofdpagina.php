ec23b382e24c65b7ce9d98312e7a27627c53e470
Bram Floris <bramfloris@gmail.com>
2016-11-08 20:14:53 +0100

    Initial Commit

---
+++ Testsite/Hoofdpagina.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+<link rel="stylesheet" href="css/main.css" />
+</head>
+
+<body>
+
+<h1>Een titel</h1>
+
+<p>Stukje tekst over dingen</>
+
+<h2>Een subtitel</h2>
+
+<p>Test voor het jaar <?php echo date(Y);?></[p>
+
+</body>
+
+</html>
